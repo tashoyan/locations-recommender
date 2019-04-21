5763ad4c814d9b94cff33bdd6c03813cc43bd3a0
Arseniy Tashoyan <tashoyan@gmail.com>
2019-04-21 09:10:52 +0300

    VisitGraphBuilder - partition by region_id

--- recommender/src/main/scala/com/github/tashoyan/visitor/recommender/PlaceSimilarPlace.scala
+++ recommender/src/main/scala/com/github/tashoyan/visitor/recommender/PlaceSimilarPlace.scala
@@ -18,6 +18,7 @@ object PlaceSimilarPlace {
   def calcPlaceSimilarPlaceEdges(placeVisits: DataFrame): DataFrame = {
     val thatPlaceVisits = placeVisits
       .select(
+        col("region_id"),
         col("person_id"),
         col("place_id") as "that_place_id",
         col("timestamp") as "that_timestamp"
@@ -27,12 +28,12 @@ object PlaceSimilarPlace {
       math.abs(ts1.getTime - ts2.getTime) <= placeSimilarityIntervalMillis
     }
     val samePersonVisitCounts = placeVisits
-      .join(thatPlaceVisits, "person_id")
+      .join(thatPlaceVisits, Seq("region_id", "person_id"))
       .where(
-        (col("place_id") =!= col("that_place_id")) and
+        col("place_id") =!= col("that_place_id") and
           isInPlaceSimilarityIntervalUdf(col("timestamp"), col("that_timestamp"))
       )
-      .groupBy("place_id", "that_place_id")
+      .groupBy("region_id", "place_id", "that_place_id")
       .agg(count("person_id") as "visit_count")
 
     val window = Window.partitionBy("place_id")
@@ -42,28 +43,28 @@ object PlaceSimilarPlace {
       .where(col("rank") <= similarPlacesTopN)
       .drop("rank")
       .cache()
-    println("topSimilarPlaces")
-    topSimilarPlaces.show(false)
-    topSimilarPlaces.printSchema()
 
     val totalPlaceVisitCounts = topSimilarPlaces
-      .groupBy("place_id")
+      .groupBy("region_id", "place_id")
       .agg(sum("visit_count") as "total_visit_count")
       // Workaround for https://issues.apache.org/jira/browse/SPARK-14948
       .withColumnRenamed("place_id", "renamed_place_id")
-    println("totalPlaceVisitCounts")
-    totalPlaceVisitCounts.show(false)
-    totalPlaceVisitCounts.printSchema()
+      .withColumnRenamed("region_id", "renamed_region_id")
 
-    //TODO How to partition?
     topSimilarPlaces
-      .join(totalPlaceVisitCounts, col("place_id") === col("renamed_place_id"))
+      .join(
+        totalPlaceVisitCounts,
+        col("place_id") === col("renamed_place_id") and
+          col("region_id") === col("renamed_region_id")
+      )
       .withColumn("weight", col("visit_count") / col("total_visit_count") cast DoubleType)
       .select(
         col("place_id") as "source_id",
         col("that_place_id") as "target_id",
-        col("weight")
+        col("weight"),
+        col("region_id")
       )
+      .repartition(col("region_id"))
   }
 
 }
