9ccd4308f7405cc9c224eaeaf706bfd071a7c1a6
Arseniy Tashoyan <tashoyan@gmail.com>
2019-04-21 06:44:33 +0300

    Use a faster commons-math3 library

--- recommender/src/main/scala/com/github/tashoyan/visitor/recommender/Location.scala
+++ recommender/src/main/scala/com/github/tashoyan/visitor/recommender/Location.scala
@@ -1,6 +1,7 @@
 package com.github.tashoyan.visitor.recommender
 
 import com.github.tashoyan.visitor.recommender.Location._
+import org.apache.commons.math3.util.FastMath._
 
 case class Location(latitude: Double, longitude: Double) {
   require(latitude >= MIN_LATITUDE && latitude <= MAX_LATITUDE, s"Latitude $latitude must be within range [$MIN_LATITUDE, $MAX_LATITUDE]")
@@ -11,7 +12,6 @@ case class Location(latitude: Double, longitude: Double) {
   }
 }
 
-//TODO FastMath from commons-math3
 object Location {
   val MIN_LATITUDE: Double = -90.0
   val MAX_LATITUDE: Double = 90.0
@@ -21,17 +21,17 @@ object Location {
   val earthRadiusMeters: Double = 6371 * 1000
 
   def distanceMeters(location1: Location, location2: Location): Double = {
-    val lat1 = math.toRadians(location1.latitude)
-    val lat2 = math.toRadians(location2.latitude)
-    val lon1 = math.toRadians(location1.longitude)
-    val lon2 = math.toRadians(location2.longitude)
+    val lat1 = toRadians(location1.latitude)
+    val lat2 = toRadians(location2.latitude)
+    val lon1 = toRadians(location1.longitude)
+    val lon2 = toRadians(location2.longitude)
     val hav = haversine(lat2 - lat1) +
-      math.cos(lat1) * math.cos(lat2) * haversine(lon2 - lon1)
-    earthRadiusMeters * 2 * math.sqrt(hav)
+      cos(lat1) * cos(lat2) * haversine(lon2 - lon1)
+    earthRadiusMeters * 2 * sqrt(hav)
   }
 
   def haversine(theta: Double): Double = {
-    val s = math.sin(theta / 2)
+    val s = sin(theta / 2)
     s * s
   }
 
