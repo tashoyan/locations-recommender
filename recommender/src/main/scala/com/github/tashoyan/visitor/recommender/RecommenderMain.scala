1500430bd1a153dd1b520cf6d004ab9aec7ac10f
Arseniy Tashoyan <tashoyan@gmail.com>
2019-04-24 19:34:23 +0300

    WIP: Per-region recommendations

--- recommender/src/main/scala/com/github/tashoyan/visitor/recommender/RecommenderMain.scala
+++ recommender/src/main/scala/com/github/tashoyan/visitor/recommender/RecommenderMain.scala
@@ -6,6 +6,8 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import scala.io.StdIn
 import scala.util.{Failure, Success, Try}
 
+//TODO Refactor
+//scalastyle:off
 object RecommenderMain extends RecommenderArgParser {
 
   def main(args: Array[String]): Unit = {
@@ -15,50 +17,109 @@ object RecommenderMain extends RecommenderArgParser {
     }
   }
 
+  def getHomeRegion(personId: Long, persons: DataFrame)(implicit spark: SparkSession): Try[Long] = {
+    import spark.implicits._
+
+    val regionIds = persons
+      .where(col("id") === personId)
+      .limit(1)
+      .select("home_region_id")
+      .as[Long]
+      .collect()
+    Try(
+      regionIds
+        .headOption
+        .getOrElse(throw new NoSuchElementException(s"Person not found: $personId"))
+    )
+  }
+
   private def doMain(implicit config: RecommenderConfig): Unit = {
     implicit val spark: SparkSession = SparkSession.builder()
       .getOrCreate()
 
     Console.out.println(s"Actual configuration: $config")
 
-    val placesFile = s"${config.samplesDir}/places_sample"
-    Console.out.println(s"Loading places from $placesFile")
-    val places = spark.read
-      .parquet(placesFile)
+    val personsFile = s"${config.samplesDir}/persons_sample"
+    Console.out.println(s"Loading persons from $personsFile")
+    val persons = spark.read
+      .parquet(personsFile)
 
-    val stochasticGraphFile = s"${config.samplesDir}/stochastic_graph"
-    Console.out.println(s"Loading stochastic graph of visited places from $stochasticGraphFile")
-    val stochasticGraph = spark.read
-      .parquet(stochasticGraphFile)
-
-    val recommender = new StochasticRecommender(
-      stochasticGraph,
-      config.epsilon,
-      config.maxIterations
-    )
     while (true) {
-      val userInput = StdIn.readLine("Enter ID of the person to be provided with recommendations (CTRL-C for exit):")
-      Try(userInput.toLong) match {
-        case Success(personId) =>
-          printRecommendations(personId, recommender, places)
+      val personIdInput = Try(
+        StdIn.readLine("Enter ID of the person to be provided with recommendations (CTRL-C for exit):")
+      )
+      val targetRegionIdInput = Try(
+        StdIn.readLine("Enter ID of the target region, leave blank for the person's home region (CTRL-C for exit):")
+      )
+      val input = for {
+        personId <- personIdInput.map(_.toLong)
+        regionIds <- calcRegionIdsForRecommender(personId, targetRegionIdInput, persons)
+      } yield (personId, regionIds)
+      input match {
+        case Success((personId, regionIds)) =>
+          printRecommendations(personId, regionIds)
         case Failure(e) =>
-          Console.err.println(s"Failed to parse the input: $userInput")
+          Console.err.println("Failed to parse the input:")
           Console.err.println(e.getClass.getSimpleName + ": " + e.getMessage)
       }
     }
   }
 
-  private def printRecommendations(personId: Long, recommender: StochasticRecommender, places: DataFrame)(implicit config: RecommenderConfig): Unit = {
+  private def calcRegionIdsForRecommender(personId: Long, regionIdInput: Try[String], persons: DataFrame)(implicit spark: SparkSession, config: RecommenderConfig): Try[Seq[Long]] = {
+    val homeRegion = getHomeRegion(personId, persons)
+    val emptyStrRegex = """\s*""".r
+    regionIdInput.flatMap {
+      case emptyStrRegex() =>
+        Console.out.println("Target region is not provided; will take the person's home region")
+        homeRegion
+          .map(Seq(_))
+      case regionIdStr =>
+        homeRegion
+          .map(Seq(_, regionIdStr.toLong))
+    }
+  }
+
+  private def printRecommendations(personId: Long, regionIds: Seq[Long])(implicit spark: SparkSession, config: RecommenderConfig): Unit = {
+    val stochasticGraph = loadStochasticGraph(regionIds)
+    val recommender = new StochasticRecommender(
+      stochasticGraph,
+      config.epsilon,
+      config.maxIterations
+    )
+    printRecommendations(personId, recommender)
+  }
+
+  private def loadStochasticGraph(regionIds: Seq[Long])(implicit spark: SparkSession, config: RecommenderConfig): DataFrame = {
+    val stochasticGraphFile = generateGraphFileName(regionIds)
+    Console.out.println(s"Loading stochastic graph of visited places from $stochasticGraphFile")
+    val stochasticGraph = spark.read
+      .parquet(stochasticGraphFile)
+    stochasticGraph
+  }
+
+  //TODO Extract to a common module and remove the duplication with StochasticGraphBuilderMain
+  private def generateGraphFileName(regionIds: Seq[Long])(implicit config: RecommenderConfig): String = {
+    regionIds
+      .map(regId => s"region$regId")
+      .mkString(s"${config.samplesDir}/stochastic_graph_", "_", "")
+  }
+
+  private def printRecommendations(personId: Long, recommender: StochasticRecommender)(implicit spark: SparkSession, config: RecommenderConfig): Unit = {
     Try(recommender.makeRecommendations(personId, config.maxRecommendations)) match {
       case Success(recommendations) =>
-        printRecommendations(personId, recommendations, places)
+        printRecommendations(personId, recommendations)
       case Failure(e) =>
         Console.err.println(s"Failed to get recommendations for person: $personId")
         Console.err.println(e.getClass.getSimpleName + ": " + e.getMessage)
     }
   }
 
-  private def printRecommendations(personId: Long, recommendations: DataFrame, places: DataFrame): Unit = {
+  private def printRecommendations(personId: Long, recommendations: DataFrame)(implicit spark: SparkSession, config: RecommenderConfig): Unit = {
+    val placesFile = s"${config.samplesDir}/places_sample"
+    Console.out.println(s"Loading places from $placesFile")
+    val places = spark.read
+      .parquet(placesFile)
+
     val recommendedPlaces = places
       .join(broadcast(recommendations), "id")
     Console.out.println(s"Person $personId might want to visit:")
