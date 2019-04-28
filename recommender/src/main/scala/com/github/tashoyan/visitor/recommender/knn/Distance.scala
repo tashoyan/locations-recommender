2f389d3e4d21eb5e4e000c33d4e86aa7ec15a198
Arseniy Tashoyan <tashoyan@gmail.com>
2019-04-28 16:04:55 +0300

    Use Spark ML vectors in Spark data frames, but use Breeze vectors to calculate similarity. Ugly Spark ML API.

--- recommender/src/main/scala/com/github/tashoyan/visitor/recommender/knn/Distance.scala
+++ recommender/src/main/scala/com/github/tashoyan/visitor/recommender/knn/Distance.scala
@@ -1,15 +1,16 @@
 package com.github.tashoyan.visitor.recommender.knn
 
-import breeze.linalg.SparseVector
+import org.apache.spark.ml.linalg.SparseVector
+import com.github.tashoyan.visitor.recommender.knn.Vectors.RichSparseVector
 
 object Distance {
 
-  def cosineSimilarity(vector1: SparseVector[Long], vector2: SparseVector[Long]): Double = {
+  def cosineSimilarity(vector1: SparseVector, vector2: SparseVector): Double = {
     (vector1 dot vector2) / (vectorLength(vector1) * vectorLength(vector2))
   }
 
-  def vectorLength(vector: SparseVector[Long]): Double = {
-    val sumSquared = vector.data
+  def vectorLength(vector: SparseVector): Double = {
+    val sumSquared = vector.values
       .map(v => v * v)
       .sum
     math.sqrt(sumSquared.toDouble)
---
+++ recommender/src/main/scala/com/github/tashoyan/visitor/recommender/knn/Vectors.scala
@@ -0,0 +1,18 @@
+package com.github.tashoyan.visitor.recommender.knn
+
+import breeze.linalg.{SparseVector => BSV}
+import org.apache.spark.ml.linalg.SparseVector
+
+object Vectors {
+
+  implicit class RichSparseVector(val vector: SparseVector) extends AnyVal {
+
+    @inline def toBreeze: BSV[Double] =
+      new BSV[Double](vector.indices, vector.values, vector.size)
+
+    @inline def dot(that: SparseVector): Double =
+      this.toBreeze dot that.toBreeze
+
+  }
+
+}
