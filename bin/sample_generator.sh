d03726270485cd14024293e1e2eade6ba9871b41
Arseniy Tashoyan <tashoyan@gmail.com>
2019-04-30 13:55:18 +0300

    Fix: Bad logging config - huge log file in /tmp

--- bin/sample_generator.sh
+++ bin/sample_generator.sh
@@ -16,13 +16,15 @@ then
 fi
 
 app_name="$(basename $0)"
-log_config_file="$base_dir/conf/log4j.xml"
-spark_config_file="$base_dir/conf/spark.conf"
+conf_dir="$base_dir/conf"
+log_config_file="$conf_dir/log4j.xml"
+spark_config_file="$conf_dir/spark.conf"
+log_dir="$base_dir/log"
 spark-submit \
 --name "$app_name" \
 --properties-file "$spark_config_file" \
---conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file://$log_config_file" \
---conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file://$log_config_file" \
+--conf "spark.driver.extraJavaOptions=-Dapp.log.dir=$log_dir -Dlog4j.configuration=file://$log_config_file" \
+--conf "spark.executor.extraJavaOptions=-Dapp.log.dir=$log_dir -Dlog4j.configuration=file://$log_config_file" \
 --class com.github.tashoyan.visitor.sample.SampleGeneratorMain \
 "$jar_file" \
 --samples-dir "$samples_dir"
