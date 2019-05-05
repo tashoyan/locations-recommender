09c1350d605dd9012ef1cc7c4cda6ca883d34da5
Arseniy Tashoyan <tashoyan@gmail.com>
2019-05-05 23:32:20 +0300

    Fix: Make logging configuration available for remote executors

--- bin/similar_persons_builder.sh
+++ bin/similar_persons_builder.sh
@@ -18,13 +18,14 @@ fi
 app_name="$(basename $0)"
 conf_dir="$base_dir/conf"
 log_config_file="$conf_dir/log4j.xml"
+remote_log_config_file="log4j.xml"
 spark_config_file="$conf_dir/spark.conf"
-log_dir="$base_dir/log"
 spark-submit \
 --name "$app_name" \
 --properties-file "$spark_config_file" \
---conf "spark.driver.extraJavaOptions=-Dapp.log.dir=$log_dir -Dlog4j.configuration=file://$log_config_file" \
---conf "spark.executor.extraJavaOptions=-Dapp.log.dir=$log_dir -Dlog4j.configuration=file://$log_config_file" \
+--files "$log_config_file#$remote_log_config_file" \
+--conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file://$remote_log_config_file" \
+--conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file://$remote_log_config_file" \
 --class com.github.tashoyan.recommender.knn.SimilarPersonsBuilderMain \
 "$jar_file" \
 --data-dir "$data_dir" \
