faeb0a52561760a8219deea3fbb9122ea0fed297
Arseniy Tashoyan <tashoyan@gmail.com>
2019-05-08 07:08:38 +0300

    Spark and logging configuration

--- bin/stochastic_recommender.sh
+++ bin/stochastic_recommender.sh
@@ -18,13 +18,15 @@ fi
 app_name="$(basename $0)"
 conf_dir="$base_dir/conf"
 log_config_file="$conf_dir/log4j.xml"
-spark_config_file="$conf_dir/spark.conf"
+remote_log_config_file="log4j.xml"
+spark_config_file="$conf_dir/spark-yarn-client.conf"
 log_dir="$base_dir/log"
 spark-submit \
 --name "$app_name" \
 --properties-file "$spark_config_file" \
+--files "$log_config_file#$remote_log_config_file" \
 --conf "spark.driver.extraJavaOptions=-Dapp.log.dir=$log_dir -Dlog4j.configuration=file://$log_config_file" \
---conf "spark.executor.extraJavaOptions=-Dapp.log.dir=$log_dir -Dlog4j.configuration=file://$log_config_file" \
+--conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=$remote_log_config_file" \
 --class com.github.tashoyan.recommender.stochastic.StochasticRecommenderMain \
 "$jar_file" \
 --data-dir "$data_dir" \
